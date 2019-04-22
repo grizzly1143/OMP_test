b4d720f5a4116507084b7d93981d6264e8fe9eda
CCB <i.krasheninnikov@innopolis.ru>
2019-04-22 22:54:22 +0300

    ! data representation

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ typedef struct {
 
 typedef struct {
         int x, y;
-        PPMPixel *data;
+        PPMPixel **data;
 } PPMImage;
 
 #define RGB_COMPONENT_COLOR 255
@@ -64,30 +64,25 @@ static PPMImage *readPPM(const char *filename)
                 exit(1);
         }
 
-        //check rgb component depth
-        if (rgb_comp_color!= RGB_COMPONENT_COLOR) {
-                fprintf(stderr, "'%s' does not have 8-bits components\n", filename);
-                exit(1);
-        }
-
         while (fgetc(fp) != '\n') ;
         //memory allocation for pixel data
-        img->data = (PPMPixel*)malloc(img->x * img->y * sizeof(PPMPixel));
-
+        img->data = (PPMPixel **)malloc(img->x * sizeof(PPMPixel*));
+        for (int i = 0; i < img->x; i++)
+                img->data[i] = malloc(img->y * sizeof(PPMPixel));
         if (!img) {
                 fprintf(stderr, "Unable to allocate memory\n");
                 exit(1);
         }
 
         //read pixel data from file
-        if (fread(img->data, 3 * img->x, img->y, fp) != img->y) {
-                fprintf(stderr, "Error loading image '%s'\n", filename);
-                exit(1);
+        for (int i = 0; i < img->x; i++) {
+                fread(img->data[i], 3,img->y, fp);
         }
 
         fclose(fp);
         return img;
 }
+
 void writePPM(const char *filename, PPMImage *img)
 {
         FILE *fp;
@@ -99,7 +94,6 @@ void writePPM(const char *filename, PPMImage *img)
         }
 
         //write the header file
-        //image format
         fprintf(fp, "P6\n");
 
         //image size
@@ -109,26 +103,45 @@ void writePPM(const char *filename, PPMImage *img)
         fprintf(fp, "%d\n",RGB_COMPONENT_COLOR);
 
         // pixel data
-        fwrite(img->data, 3 * img->x, img->y, fp);
+        for (int i = 0; i < img->x; i++) {
+                fwrite(img->data[i], 3 * sizeof(unsigned char),img->y, fp);
+        }
         fclose(fp);
 }
-
-void changeColorPPM(PPMImage *img)
+/*
+void sobel_operator(PPMImage *img. PPMImage *img2)
 {
         int i;
         if(img){
-                for(i=0;i<img->x*img->y;i++){
+                int * pixels = malloc(9 * sizeof(unsigned char))
+                for(i = 0; i < img->x * img->y; i++){
+                        if (i % img->x == 0) {
+                                pixels[0] = 0;
+                                pixels[3] = 0;
+                                pixels[6] = 0;
+                        }
+                        if (i < img->x) {
+                                pixels[0] = 0;
+                                pixels[1] = 0;
+                                pixels[2] = 0;
+                        }
+                        if ((i % img->x) == img->x - 1) {
+                                pixels[2] = 0;
+                                pixels[5] = 0;
+                                pixels[8] = 0;
+                        }
                         img->data[i].red=RGB_COMPONENT_COLOR-img->data[i].red;
                         img->data[i].green=RGB_COMPONENT_COLOR-img->data[i].green;
                         img->data[i].blue=RGB_COMPONENT_COLOR-img->data[i].blue;
                 }
         }
 }
-
+*/
 int main(){
         PPMImage *image;
         image = readPPM("img.ppm");
-        writePPM("can_bottom2.ppm",image);
+        PPMImage *out_image;
+        //writePPM("out.ppm",image);
         printf("Press any key...");
         getchar();
 }
